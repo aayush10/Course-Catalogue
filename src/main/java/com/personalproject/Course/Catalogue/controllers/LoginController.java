634fcb84e63e0725b8d05725be3bf4d2500aafea
Aayush <sharma.aayush42@gmai.com>
2021-12-20 23:23:45 +0530

    added validation to the register form

--- src/main/java/com/personalproject/Course/Catalogue/controllers/LoginController.java
+++ src/main/java/com/personalproject/Course/Catalogue/controllers/LoginController.java
@@ -2,6 +2,7 @@ package com.personalproject.Course.Catalogue.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -16,5 +17,8 @@ public class LoginController {
         return "register_user";
     }
 
-
+    @PostMapping("/register")
+    public void submitUser(){
+        System.out.println("user registered");
+    }
 }
